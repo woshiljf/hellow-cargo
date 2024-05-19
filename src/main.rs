b8c90379b483d09584a30c3afc5f9afa8cb4b31c
longdahai <1741222946@qq.com>
2024-05-19 14:58:15 +0800

    feat: 循环

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,30 @@ fn main() {
     println!("Hello, world!ffff");
     println!("请输入你的猜测数");   
 
-    let mut guess = String::new();
     let msg = "猜测错误";
     let secret_number  = rand::thread_rng().gen_range(1..101);
-
-    io::stdin().read_line(&mut guess).expect(msg);
-
     println!("随机数是{}", secret_number);  
-
-    // 可以重复声明同名的变量，会覆盖上一个变量
-    let guess: u32 = guess.trim().parse().expect(msg);
-
-    match guess.cmp(&secret_number) {
-
-        Ordering::Less => println!("猜小了"),
-        Ordering::Greater => println!("猜大了"),
-        Ordering::Equal => println!("猜对了"),
-        
+    loop {
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).expect(msg);
+    
+    
+        // 可以重复声明同名的变量，会覆盖上一个变量
+        let guess: u32 = guess.trim().parse().expect(msg);
+    
+        match guess.cmp(&secret_number) {
+    
+            Ordering::Less => println!("猜小了"),
+            Ordering::Greater => println!("猜大了"),
+            Ordering::Equal => {
+                println!("猜对了");
+                break;
+            },
+            
+        }
+    
     }
 
 
-
-    // {}，相当于printf的%s，占据位置
-    print!("你的猜测数是{}", guess)
 }
