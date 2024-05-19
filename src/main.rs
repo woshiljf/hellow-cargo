7349c79d7b25a1bc85698add50dbd48fdbc06a58
longdahai <1741222946@qq.com>
2024-05-19 14:15:34 +0800

    feat: rust 基本库的引用和变量声明

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+// use 使用其他的库进来
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!ffff");
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("错误了");
+    let msg = "猜测错误";
+    io::stdin().read_line(&mut guess).expect(msg);
+    // {}，相当于printf的%s，占据位置
     print!("你的猜测数是{}", guess)
 }
