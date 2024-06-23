2092394c741966e0793b1bf58c6c577432cf4a29
longdahai <1741222946@qq.com>
2024-06-23 15:57:29 +0800

    fix: 函数返回值

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
-
 // 入口函数
 
-use std::string;
-
 fn main() {
-    let mut guess = String::from("我是大海沟");
-    get_name(&mut guess);
-    print!("{}", guess)
+    let guess = 10;
+    let age = get_name(guess);
+    print!("{}", age)
 }
 
-fn get_name(name: &mut String) {
-
-    print!("{}", name);
-    // name 只是借用，所有没有权利修改;
-    // name.push_str("测试一下");
-
-    // 可以修改
-    name.push_str("测试一下");   
-    name.len();
+fn get_name(age: i32) -> i32 {
+    age + 20
 }
-
