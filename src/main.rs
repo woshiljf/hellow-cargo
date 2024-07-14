3b21555e17e2e354637b1eb1658e1ebf384ba53e
longdahai <1741222946@qq.com>
2024-07-14 20:54:07 +0800

    fix: options

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 mod struct623;
 mod struct_rectangle623;
 mod enum624;
+mod options714;
+
 fn main() {
     // 623：结构体
 //    let mut user = struct623::lean_struct();
 //    let mut rect = struct_rectangle623::enter_struct();
 //    // 624：枚举
    enum624::testEnum();
+   options714::testOptions()
 
    
 //    panic!("{}", user.email);
---
+++ src/options714.rs
@@ -0,0 +1,12 @@
+pub fn testOptions() {
+
+    let x = (5);
+    let y = (6);
+
+    let absent_number: Option<i32> = None;
+
+    let a  = x + y;
+
+    print!("{:#?}", a);
+
+ }
